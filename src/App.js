3f86e7fc0d8f3613e0df2fa2de5ef4905fc80a18
Julian Gonzalez <jgonz156@lion.lmu.edu>
2020-04-16 00:48:25 -0700

    Added Movement to Small On Screen Character

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './features/map'
+import Player from "./features/player"
 
 function App() {
   return (
     <div>
-      <Map/>
+          <Map />
+          <Player />
     </div>
   );
 }
